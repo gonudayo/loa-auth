ca2a685947d1e2427af8844d64a320cdbf16228f
gonudayo <kgw010119@gmail.com>
2024-12-16 20:05:19 +0900

    set nodemon

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
 const app = express();
-const port = 3000;
+dotenv.config();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+module.exports = app;
